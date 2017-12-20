405e0cf6ab1463826379f39542cea97ae51df5ad
Przemyslaw Jan Pietrzak <przemyslaw.jan.pietrzak@gmail.com>
2017-12-20 13:15:59 +0100

    add find by author feature

--- api/managers.py
+++ api/managers.py
@@ -6,16 +6,27 @@ from text_recognition.main import parse_question
 from pony.orm import select, db_session
 
 
-@db_session
-def find_painting(name):
+def _get_query(incident_name, entity_value):
+    if incident_name == 'find_by_title':
+        return lambda paint: paint.title == entity_value
+    if incident_name == 'find_by_author':
+        return lambda paint: entity_value in paint.author
 
-    parsed_question = parse_question(name)
+
+@db_session
+def find_painting(question):
+
+    parsed_question = parse_question(question)
 
     incident_name = parsed_question['intent']['name']
+    entity = parsed_question['entities']
+    if not entity:
+        raise PaintingNotFound()
+
     entity_value = parsed_question['entities'][0]['value']
     
     paint = (Paint
-        .select(lambda paint: paint.title == entity_value)
+        .select(_get_query(incident_name, entity_value))
         .first())
 
     if paint is None:
--- api/tests.py
+++ api/tests.py
@@ -16,7 +16,11 @@ def test_painting_not_found(client):
 
 
 def test_find_painting_by_name(client):
-    result = client.simulate_get('/painting/show%20me%20Allegory')
+    result = client.simulate_get('/painting/show me Allegory')
     assert result.status_code == 200
     assert result.content == b'https://www.wga.hu/html/a/aachen/allegory.html'
 
+def test_find_painting_by_title(client):
+    result = client.simulate_get('/painting/show me a picture painted by BLAKE')
+    assert result.status_code == 200
+    assert result.content == b'https://www.wga.hu/html/b/blake/00lear.html'
