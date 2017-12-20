a842c85e644fea050c3b5d2894691e87e65a4b5e
Przemyslaw Jan Pietrzak <przemyslaw.jan.pietrzak@gmail.com>
2017-12-20 14:11:59 +0100

    set fixed model name

--- text_recognition/main.py
+++ text_recognition/main.py
@@ -3,7 +3,7 @@ from rasa_nlu.config import RasaNLUConfig
 
 
 config = RasaNLUConfig("text_recognition/config.json")
-interpreter = Interpreter.load('./text_recognition/models/default/model_20171220-020007/', config)
+interpreter = Interpreter.load('./text_recognition/models/default/model/', config)
 
 
 def parse_question(question):
--- text_recognition/train.py
+++ text_recognition/train.py
@@ -1,8 +1,11 @@
 from rasa_nlu.converters import load_data
 from rasa_nlu.config import RasaNLUConfig
+from rasa_nlu.components import ComponentBuilder
 from rasa_nlu.model import Trainer
 
+
+builder = ComponentBuilder(use_cache=True)  
 training_data = load_data('text_recognition/data.json')
-trainer = Trainer(RasaNLUConfig('text_recognition/config.json'))
+trainer = Trainer(RasaNLUConfig('text_recognition/config.json'), builder)
 trainer.train(training_data)
-model_directory = trainer.persist('text_recognition/models/')
+model_directory = trainer.persist('text_recognition/models/', fixed_model_name='model')
