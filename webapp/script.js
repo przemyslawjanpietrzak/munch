4395878ab7f42b3081819fb3b0bdf5668dbd684c
Przemyslaw Jan Pietrzak <przemyslaw.jan.pietrzak@gmail.com>
2018-03-09 11:19:37 +0100

    webapp: handle user input

---
+++ webapp/script.js
@@ -0,0 +1,29 @@
+(function() {
+    'use strict';
+
+    const getMessage = (message, messageType) => 
+`<div class="message-wrapper ${ messageType }">
+    <div class="circle-wrapper animated bounceIn"></div>
+    <div class="text-wrapper animated fadeIn">${ message }</div>
+</div>`;
+
+    const content = document.getElementById('content');
+    const input = document.getElementById('input');
+    const sendButton = document.getElementById('send');
+    const inner = document.getElementById('inner');
+    
+
+    sendButton.addEventListener('click', () => {
+        const message = input.value;
+        if (!message) {
+            return;
+        }
+        const element = document.createElement('div');
+        element.innerHTML = getMessage(message, 'me');
+        content.appendChild(element);
+        input.value = '';
+        inner.scroll({ top: inner.scrollHeight });
+        input.focus();
+    });
+
+})()
