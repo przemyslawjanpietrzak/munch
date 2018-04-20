126195c5e7d90cb6e6a17335d32eb1c2e71000aa
Przemyslaw Jan Pietrzak <przemyslaw.jan.pietrzak@gmail.com>
2018-04-20 13:15:12 +0200

    frontend: integrate http

--- frontend/main.elm
+++ frontend/main.elm
@@ -3,14 +3,26 @@ module Main exposing (..)
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onInput, onClick)
+import Http
+import Json.Decode as Decode
 
 
 main =
-    Html.beginnerProgram
-        { model = model
-        , view = view
+    Html.program
+        {
+        view = view
         , update = update
+        ,init = init
+        , subscriptions = subscriptions
         }
+
+
+init : ( Model, Cmd Msg )
+init  =
+    (
+        { messages = [], inputField = "show me allegory" }
+        , Cmd.none
+    )
 
 
 
@@ -41,18 +53,45 @@ model =
 type Msg
     = Content String
     | Send
+    | Response (Result Http.Error String)
 
 
-update : Msg -> Model -> Model
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         Content content ->
-            { model | inputField = content }
+            ( { model | inputField = content }, Cmd.none )
 
         Send ->
-            { inputField = ""
-            , messages = { content = model.inputField, isBot = False } :: model.messages
-            }
+            ( { inputField = ""
+              , messages = { content = model.inputField, isBot = False } :: model.messages
+              }
+            , getRandomGif model.inputField
+            )
+
+        Response (Ok newUrl) ->
+            ( model, Cmd.none )
+
+        Response (Err _) ->
+            ( model, Cmd.none )
+
+
+
+-- Http
+
+
+getRandomGif : String -> Cmd Msg
+getRandomGif topic =
+    let
+        url =
+            "http://munch.today/inex.html" ++ topic
+    in
+        Http.send Response (Http.get url decodeGifUrl)
+
+
+decodeGifUrl : Decode.Decoder String
+decodeGifUrl =
+    Decode.at [ "data", "image_url" ] Decode.string
 
 
 
@@ -83,3 +122,12 @@ view model =
         , showMessages (model.messages)
         , button [ onClick Send ] [ text "send" ]
         ]
+
+
+
+-- subscriptions
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
