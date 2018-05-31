16012293d36615d53935bd043aac26190e054cea
Przemyslaw Jan Pietrzak <przemyslaw.jan.pietrzak@gmail.com>
2018-05-31 21:47:20 +0200

    move static files  to nginx

--- backend/api/views.py
+++ backend/api/views.py
@@ -17,18 +17,6 @@ class PaintingView:
             response.status = falcon.HTTP_404
 
 
-class StaticResource:
-
-    def on_get(self, req, resp, filename):
-        if filename not in ['index.html', 'index.js']:
-            resp.status = falcon.HTTP_403
-            return
-        resp.status = falcon.HTTP_200
-        resp.content_type = 'text/html' if filename == 'index.html' else 'text/css'
-        with open('dist/{}'.format(filename), 'r') as f:
-            resp.body = f.read()
-
-
 class MainView:
 
     def on_get(self, req, resp):
